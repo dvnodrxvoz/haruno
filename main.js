08d40f876ba525ec51c483a440c5d4583d69c536
dvnodrxvoz <90953906+dvnodrxvoz@users.noreply.github.com>
2021-10-15 19:38:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ global.reloadHandler = function () {
     conn.off('CB:action,,call', conn.onCall)
   }
   conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject\n\n@desc'
-  conn.bye = 'Selamat tinggal @user'
+  conn.bye = 'Selamat jalan @user semoga tenang disana'
   conn.spromote = '@user sekarang admin'
   conn.sdemote = '@user sekarang bukan admin'
   conn.handler = handler.handler
