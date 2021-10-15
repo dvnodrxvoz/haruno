60dc8034dca858d59434b6ad84b0d7a8471695c9
dvnodrxvoz <90953906+dvnodrxvoz@users.noreply.github.com>
2021-10-15 19:39:45 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 628112958665
-├ OVO: 628112958665
-├ Dana: 628112958665
-├ Pulsa (XL): 6281943265086
+├ GoPay: 6285773744917
+├ OVO: 6285773744917
+├ Dana: 6285773744917
+├ Pulsa (IM3): 6285773744917
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
