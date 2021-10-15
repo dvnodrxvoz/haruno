5046bc208b9fdffce30c1a73d6032699ac88f813
dvnodrxvoz <90953906+dvnodrxvoz@users.noreply.github.com>
2021-10-15 20:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['628112958665', '6288215569001', '628998512588', '6283872123464', '628996679822', '6285693602003'] // Letakan nomor kamu disini
+global.owner = ['6283872123464', '628996679822'] // Letakan nomor kamu disini
 global.mods = [''] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
